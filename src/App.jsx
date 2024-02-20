37fdef41635d140228b6974290c89aab736edc7d
ksturgeongcc <ksturgeongcc@gmail.com>
2024-02-20 12:09:00 +0000

    adding changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import Landing from './pages/shared/Landing'
-import './App.css'
+// src/App.js
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
 
+const App = () => {
   return (
-    <>
-      <Landing />
-    </>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App
+        <hr />
+
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
