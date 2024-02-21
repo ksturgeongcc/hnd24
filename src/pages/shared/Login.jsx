02a1b4f55e64ac393d924f6e0edef2ac36f931c4
ksturgeongcc <ksturgeongcc@gmail.com>
2024-02-21 10:05:06 +0000

    adding css

--- src/pages/shared/Login.jsx
+++ src/pages/shared/Login.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './login.css';
 
 const Login = () => {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Login</h1>
+            <h1 className="text-3xl font-bold red underline">Login</h1>
         </>
     )
 }
